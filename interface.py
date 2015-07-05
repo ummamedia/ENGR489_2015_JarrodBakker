23dd26e5a17b2c7e65c60c456cfeb65c1d67c796
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2015-07-05 17:41:17 +1200

    Adjusting the interface fo threading with acl_switch.py. However it appears that another interface option will need to be considered. A program which makes RESTful API calls to acl_switch.py will probably be the way to go.

--- interface.py
+++ interface.py
@@ -10,8 +10,7 @@
 
 import sys
 import threading
-
-# TODO have this running in another thread, started by acl_switch
+import acl_switch
 
 TEXT_HELP = "To add a block rule:\n<-a or --add> <client address> <host address> <transport layer protocol> <source port> <destination port>"
 TEXT_ERROR_SYNTAX = "ERROR: Incorrect syntax, could not process given command."
@@ -19,30 +18,32 @@ SIZE_ADD_RULE = 6 # number of items expected in user's input when they create a 
 
 
 class interfaceThread (threading.Thread):
-    def __init__(self, threadID, name):
+    def __init__(self, threadID, name, acl_sw):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.acl_sw = acl_sw
 
     def run(self):
         print TEXT_HELP
         while True:
             result = evaluateInput(textInput())
-            print result
             if result[0] == "HELP":
                 print result
             elif result[0] == "ADD":
-                print result
+                # print result
                 # TODO add_flow then push out to switches
+                acl_switch.ACLSwitch.addACLRule(self.acl_sw, result[1], result[2], result[3],
+                                  result[4], result[5])
             elif result[0] == "QUIT":
-                print "HEY"
                 break
             else:
                 print TEXT_HELP
+        print "INFO: Closing interface."
         sys.exit(0)
-
         
 # Return input from the user
+# @return - the raw text input from the terminal
 def textInput():
     return raw_input()
 
@@ -60,13 +61,12 @@ def evaluateInput(buf_in):
                    buf_proc[4],buf_proc[5])
     elif (buf_proc[0] == "-q" or buf_proc[0] == "--quit"):
         buf_out = ("QUIT", "") # empty string needed as the tuple cannot have a length of 1
-        print buf_out
     else:
         buf_out = TEXT_ERROR_SYNTAX + "\n" + TEXT_HELP
     return buf_out
 
 # Main function for the interface
-def main():
+def interfaceLoop():
     print TEXT_HELP
     while True:
         result = evaluateInput(textInput())
@@ -75,11 +75,15 @@ def main():
         elif result[0] == "ADD":
             print result
             # TODO add_flow then push out to switches
+            acl_switch.ACLSwitch.addACLRule(self.acl_sw, result[1],
+                                            result[2], result[3],
+                                            result[4], result[5])
         elif result [0] == "QUIT":
-           sys.exit(0) 
+            sys.exit(0) 
         else:
             print TEXT_HELP
 
+# Start the interface in a single thread
 if __name__ == "__main__":
     thread_interface = interfaceThread(1, "Thread-Interface")
     thread_interface.start()

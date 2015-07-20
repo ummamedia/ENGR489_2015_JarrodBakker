37661584e9b5b39c41a7aeafa14d741ac4b4d2ea
Jarrod N. Bakker <jarrodbakker@hotmail.com>
2015-07-20 22:40:20 +1200

    Reimplementing interface for making RESTful API calls. Skeleton is there. Need to get it checking rule syntax before it can send to them to ACLSwitch.

--- interface.py
+++ interface.py
@@ -1,89 +1,94 @@
 # An interactive command-line based interface for rule management of the
-# Stateless Firewall (ACL) SDN application.
+# Stateless SDN Firewall application. The interface
 #
 # The interface currently assumes that the user understands the syntax
 # completely and does not check that input has been correctly provided.
 #
 # Author: Jarrod N. Bakker
 #
-#
 
+# Libraries
 import sys
-import threading
-import acl_switch
+import rule_syntax
 
-TEXT_HELP = "To add a block rule:\n<-a or --add> <client address> <host address> <transport layer protocol> <source port> <destination port>"
+# Constants
+MODE_ADD = "ADD"
+MODE_DELETE = "DELETE"
+MODE_MAIN = "MAIN"
 TEXT_ERROR_SYNTAX = "ERROR: Incorrect syntax, could not process given command."
-SIZE_ADD_RULE = 6 # number of items expected in user's input when they create a rule
+TEXT_HELP_MAIN = "\tCommands: add, delete, show, help, quit"
+TEXT_HELP_ADD = "\tRule to add: ip_src ip_dst transport_protocol port_src port_dst"
+TEXT_HELP_DELETE = "\tRule to delete: rule_id"
+PROMPT_MAIN = "ACL Switch > "
+PROMPT_ADD = "ACL Switch (add) > "
+PROMPT_DELETE = "ACL Switch (delete) > "
 
-
-class interfaceThread (threading.Thread):
-    def __init__(self, threadID, name, acl_sw):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.acl_sw = acl_sw
-
-    def run(self):
-        print TEXT_HELP
-        while True:
-            result = evaluateInput(textInput())
-            if result[0] == "HELP":
-                print result
-            elif result[0] == "ADD":
-                # print result
-                # TODO add_flow then push out to switches
-                acl_switch.ACLSwitch.addACLRule(self.acl_sw, result[1], result[2], result[3],
-                                  result[4], result[5])
-            elif result[0] == "QUIT":
-                break
-            else:
-                print TEXT_HELP
-        print "INFO: Closing interface."
-        sys.exit(0)
-        
 # Return input from the user
+# @param intMode - current mode of the interface e.g. add
 # @return - the raw text input from the terminal
-def textInput():
-    return raw_input()
+def text_input(intMode):
+    if intMode == MODE_ADD:
+        return raw_input(PROMPT_ADD)
+    elif intMode == MODE_DELETE:
+        return raw_input(PROMPT_DELETE)
+    else:
+        return raw_input(PROMPT_MAIN)
+
+# Main interface. This is presented on start-up and after an operation
+# has been processed.
+def interface_main():
+    while True:
+        result = evaluate_input(text_input(MODE_MAIN))
+        if result == "HELP":
+            print TEXT_HELP_MAIN
+        elif result == "ADD":
+            interface_add()
+        elif result == "DELETE":
+            interface_delete()
+        elif result == "SHOW":
+            # TODO display contents of the ACL
+            print "TO BE IMPLEMENTED"
+        elif result == "QUIT":
+            sys.exit(0) 
+        else:
+            print result
+
+# TODO add rule to the ACL
+def interface_add():
+    print TEXT_HELP_ADD
+    result = text_input(MODE_ADD)
+    # check that enough items were passed
+    # check that addresses are valid e.g. valid IPv4 address or is '*'
+    # check that transport protocol is valid: TCP, UDP or '*'
+    # check that port numbers are valid e.g. port number is within valid range or is '*'
+    print result
+
+# TODO delete rule from the ACL
+def interface_delete():
+    print TEXT_HELP_DELETE
+    result = text_input(MODE_DELETE)
+    print result
 
 # Evaluate the action based on the input given by the user
 # @param buf_in - input from the user
 # @return buf_out - the output to be returned
-def evaluateInput(buf_in):
+def evaluate_input(buf_in):
     buf_proc = buf_in.split(" ") # the input to process
     buf_out = None
-    if buf_proc[0] == "-h" or buf_proc[0] == "--help":
-        buf_out = ("HELP", TEXT_HELP)
-    elif (buf_proc[0] == "-a" or buf_proc[0] == "-add") and len(buf_proc) == SIZE_ADD_RULE:
-        buf_out = buf_proc
-        buf_out = ("ADD", buf_proc[1], buf_proc[2], buf_proc[3],
-                   buf_proc[4],buf_proc[5])
-    elif (buf_proc[0] == "-q" or buf_proc[0] == "--quit"):
-        buf_out = ("QUIT", "") # empty string needed as the tuple cannot have a length of 1
+    if buf_proc[0] == "help":
+        buf_out = "HELP"
+    elif buf_proc[0] == "add":
+        buf_out = "ADD"
+    elif buf_proc[0] == "delete":
+        buf_out = "DELETE"
+    elif buf_proc[0] == "show":
+        buf_out = "SHOW"
+    elif buf_proc[0] == "quit":
+        buf_out = "QUIT" # empty string needed as the tuple cannot have a length of 1
     else:
-        buf_out = TEXT_ERROR_SYNTAX + "\n" + TEXT_HELP
+        buf_out = TEXT_ERROR_SYNTAX + "\n" + TEXT_HELP_MAIN # syntax error
     return buf_out
-
-# Main function for the interface
-def interfaceLoop():
-    print TEXT_HELP
-    while True:
-        result = evaluateInput(textInput())
-        if result[0] == "HELP":
-            print result
-        elif result[0] == "ADD":
-            print result
-            # TODO add_flow then push out to switches
-            acl_switch.ACLSwitch.addACLRule(self.acl_sw, result[1],
-                                            result[2], result[3],
-                                            result[4], result[5])
-        elif result [0] == "QUIT":
-            sys.exit(0) 
-        else:
-            print TEXT_HELP
 
 # Start the interface in a single thread
 if __name__ == "__main__":
-    thread_interface = interfaceThread(1, "Thread-Interface")
-    thread_interface.start()
+    interface_main()

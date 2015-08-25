21e63ea747fcd526d7b804fb1ac5c8086ab8c5ae
Jarrod Bakker <jarrodbakker@hotmail.com>
2015-08-25 18:22:13 +1200

    Syntax now allows roles to be associated with rules.

--- rule_syntax.py
+++ rule_syntax.py
@@ -7,18 +7,20 @@
 # Author: Jarrod N. Bakker
 #
 
-#import socket, struct
 from netaddr import IPAddress
 
-# Check the ACL rule is valid.
-# @param ip_src - the IP address to check
-# @param ip_dst - the IP address to check
-# @param tp_proto - the transport layer (layer 4) protocol to check
-# @param port_src - the source port to check
-# @param port_dst - the destination port to check
-# @return - a list of the error messages. An empty list means that all
-#           tests passed and the rule is valid.
-def check_rule(ip_src, ip_dst, tp_proto, port_src, port_dst):
+"""
+Check the ACL rule is valid.
+
+@param ip_src - the IP address to check
+@param ip_dst - the IP address to check
+@param tp_proto - the transport layer (layer 4) protocol to check
+@param port_src - the source port to check
+@param port_dst - the destination port to check
+@return - a list of the error messages. An empty list means that all
+          tests passed and the rule is valid.
+"""
+def check_rule(ip_src, ip_dst, tp_proto, port_src, port_dst, role):
     errors = []
     ip_src_result = check_ip(ip_src)
     ip_dst_result = check_ip(ip_dst)
@@ -39,11 +41,16 @@ def check_rule(ip_src, ip_dst, tp_proto, port_src, port_dst):
         errors.append("Unsupported rule: transport protocol: " + tp_proto +
                       " source port: " + port_src + " destination port: " +
                       port_dst)
+    if not check_role(role):
+        errors.append("Invalid role provided: " + role)
     return errors
 
-# Check that a valid IP (v4 or v6) address has been specified.
-# @param address - the IP address to check.
-# @return - True if valid, False if not valid.
+"""
+Check that a valid IP (v4 or v6) address has been specified.
+
+@param address - the IP address to check.
+@return - True if valid, False if not valid.
+"""
 def check_ip(address):
     try:
         addr = IPAddress(address)
@@ -53,24 +60,33 @@ def check_ip(address):
             return True
         return False
 
-# Check that the source and destination IP addresses are of the same versions.
-# @param ip_src - the source IP address to check.
-# @param ip_dst - the destination IP address to check.
-# @return - True if valid, False if not valid.
+"""
+Check that the source and destination IP addresses are of the same versions.
+
+@param ip_src - the source IP address to check.
+@param ip_dst - the destination IP address to check.
+@return - True if valid, False if not valid.
+"""
 def check_ip_versions(ip_src, ip_dst):
     return IPAddress(ip_src).version == IPAddress(ip_dst).version
 
-# ACLSwtich can block all traffic (denoted by tp_proto == "*") or by
-# checking TCP or UDP port numbers. This function checks that the specified
-# transport layer (layer 4) protocol is either "*", TCP or UDP.
-# @param protocol - the transport layer (layer 4) protocol to check
-# @return - True if valid, False if not valid.
+"""
+ACLSwtich can block all traffic (denoted by tp_proto == "*") or by
+checking TCP or UDP port numbers. This function checks that the specified
+transport layer (layer 4) protocol is either "*", TCP or UDP.
+
+@param protocol - the transport layer (layer 4) protocol to check
+@return - True if valid, False if not valid.
+"""
 def check_transport_protocol(protocol):
     return (protocol == "tcp" or protocol == "udp" or protocol == "*")
 
-# A port is valid if it is either "*" or between 0 and 65535 inclusive
-# @param port - the port number to check
-# @return - True if valid, False if not valid.
+"""
+A port is valid if it is either "*" or between 0 and 65535 inclusive
+
+@param port - the port number to check
+@return - True if valid, False if not valid.
+"""
 def check_port(port):
     try:
         int(port)
@@ -82,25 +98,37 @@ def check_port(port):
             return True
         return False
 
-# An OFPMatch cannot have both TCP and UDP information in it. Therefore
-# an ACL rule is not valid if the tp_proto is "*" and port numbers are
-# specified.
-# @param tp_proto - the transport layer (layer 4) protocol to check
-# @param port_src - the source port to check
-# @param port_dst - the destination port to check
-# @return - True if valid, False if not valid.
+"""
+An OFPMatch cannot have both TCP and UDP information in it. Therefore
+an ACL rule is not valid if the tp_proto is "*" and port numbers are
+specified.
+
+@param tp_proto - the transport layer (layer 4) protocol to check
+@param port_src - the source port to check
+@param port_dst - the destination port to check
+@return - True if valid, False if not valid.
+"""
 def check_transport_valid(tp_proto, port_src, port_dst):
     return not(tp_proto == "*" and (port_src != "*" or port_dst != "*"))
+
+"""
+Only "df" or "gw" can be assigned as roles currently.
+
+@param role - the role to check
+@return - True if valid, False if not valid.
+"""
+def check_role(role):
+    return (role == "gw" or role == "df")
 
 if __name__ == "__main__":
     while(1):
         buf_in = raw_input("Rule: ")
         items = buf_in.split(" ")
         items[2] = items[2].lower()
-        if len(items) != 5:
-            print "Expected 5 arguments, " + str(len(items)) + " given."
+        if len(items) != 6:
+            print "Expected 6 arguments, " + str(len(items)) + " given."
             continue
-        errors = check_rule(items[0], items[1], items[2], items[3], items[4])
+        errors = check_rule(items[0], items[1], items[2], items[3], items[4], items[5])
         if len(errors) != 0 :
             print "Invalid rule provided:"
             for e in errors:
